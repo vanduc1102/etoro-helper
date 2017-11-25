b677ea87dbb3f24084ded204db858c8335ab6026
Duc Nguyen <vanducld@gmail.com>
2017-11-25 17:09:48 +0700

    Add card element

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
 (function () {
-  let portfolioEl;
-  let marketEl;
   function getOptions() {
     return {
       refreshTime: 10000
@@ -10,22 +8,47 @@
   }
 
   function initWatchListView() {
-    let tabletRowEls = marketEl.querySelectorAll('.table-body .table-row');
-    tabletRowEls.forEach((el, index) => {
-      if (el.className.indexOf('empty') !== -1) {
-        return;
+    try {
+      let mainContentEl = document.querySelector('.main-app-view .main-content');
+      let isListView = mainContentEl.className.indexOf('list-view') > 0;
+      if (isListView) {
+        let marketEl = mainContentEl.querySelector('.table-body.market');
+        let tabletRowEls = marketEl.querySelectorAll('.table-body .table-row');
+        tabletRowEls.forEach((el, index) => {
+          if (el.className.indexOf('empty') !== -1) {
+            return;
+          }
+          let cellNameEl = el.querySelector('.table-name-cell');
+          let tableInfoEl = el.querySelector('.table-info');
+          let sellBtnEl = tableInfoEl.querySelector('.etoro-sell-button');
+          let sellPrice = sellBtnEl.querySelector('.etoro-price-value').textContent.trim();
+          let buyBtnEl = tableInfoEl.querySelector('.etoro-buy-button');
+          let buyPrice = buyBtnEl.querySelector('.etoro-price-value').textContent.trim();
+          let spreadPrice = Number(buyPrice) - Number(sellPrice);
+          let spreadPercent = (spreadPrice / buyPrice) * 100;
+          let spreadAndPrice = '$' + toFixed(spreadPrice) + ' - ' + spreadPercent.toFixed(3) + '%';
+          createSpanNode(cellNameEl, spreadAndPrice);
+        });
+      } else {
+        let cardListEls = mainContentEl.querySelectorAll('.market-card-ph.pointer');
+        cardListEls.forEach((cardEl, index) => {
+          let btnSellEl = cardEl.querySelector('.etoro-sell-button');
+          let btnBuyEl = cardEl.querySelector('.etoro-buy-button');
+          if (!btnBuyEl || !btnSellEl) {
+            return;
+          }
+          let sellPrice = btnSellEl.querySelector('.etoro-price-value').textContent.trim();
+          let buyPrice = btnBuyEl.querySelector('.etoro-price-value').textContent.trim();
+          let marketHeadEl = cardEl.querySelector('.market-card-head');
+          let spreadPrice = Number(buyPrice) - Number(sellPrice);
+          let spreadPercent = (spreadPrice / buyPrice) * 100;
+          let spreadAndPrice = '$' + toFixed(spreadPrice) + ' - ' + spreadPercent.toFixed(3) + '%';
+          createSpanNode(marketHeadEl, spreadAndPrice);
+        });
       }
-      let cellNameEl = el.querySelector('.table-name-cell');
-      let tableInfoEl = el.querySelector('.table-info');
-      let sellBtnEl = tableInfoEl.querySelector('.etoro-sell-button');
-      let sellPrice = sellBtnEl.querySelector('.etoro-price-value').textContent.trim();
-      let buyBtnEl = tableInfoEl.querySelector('.etoro-buy-button');
-      let buyPrice = buyBtnEl.querySelector('.etoro-price-value').textContent.trim();
-      let spreadPrice = Number(buyPrice) - Number(sellPrice);
-      let spreadPercent = (spreadPrice / buyPrice) * 100;
-      let spreadAndPrice = '$' + toFixed(spreadPrice) + ' - ' + spreadPercent.toFixed(3) + '%';
-      createSpanNode(cellNameEl, spreadAndPrice);
-    });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   function createSpanNode(parent, text) {
@@ -50,21 +73,26 @@
   }
 
   function initialPortfolioOverview() {
-    let tabletRowEls = portfolioEl.querySelectorAll('.ui-table-row-container');
-    tabletRowEls.forEach((el, index) => {
-      if (el.className.indexOf('empty') !== -1) {
-        return;
-      }
-      let cellNameEl = el.querySelector('.ui-table-static-cell');
-      let sellBtnEl = el.querySelector('.etoro-sell-button');
-      let sellPrice = sellBtnEl.querySelector('.etoro-price-value').textContent.trim();
-      let buyBtnEl = el.querySelector('.etoro-buy-button');
-      let buyPrice = buyBtnEl.querySelector('.etoro-price-value').textContent.trim();
-      let spreadPrice = Number(buyPrice) - Number(sellPrice);
-      let spreadPercent = (spreadPrice / buyPrice) * 100;
-      let spreadAndPrice = '$' + toFixed(spreadPrice) + ' - ' + spreadPercent.toFixed(3) + '%';
-      createSpanNode(cellNameEl, spreadAndPrice);
-    });
+    try {
+      let portfolioEl = document.querySelector('.main-app-view .p-portfolio .portfolio-overview');
+      let tabletRowEls = portfolioEl.querySelectorAll('.ui-table-row-container');
+      tabletRowEls.forEach((el, index) => {
+        if (el.className.indexOf('empty') !== -1) {
+          return;
+        }
+        let cellNameEl = el.querySelector('.ui-table-static-cell');
+        let sellBtnEl = el.querySelector('.etoro-sell-button');
+        let sellPrice = sellBtnEl.querySelector('.etoro-price-value').textContent.trim();
+        let buyBtnEl = el.querySelector('.etoro-buy-button');
+        let buyPrice = buyBtnEl.querySelector('.etoro-price-value').textContent.trim();
+        let spreadPrice = Number(buyPrice) - Number(sellPrice);
+        let spreadPercent = (spreadPrice / buyPrice) * 100;
+        let spreadAndPrice = '$' + toFixed(spreadPrice) + ' - ' + spreadPercent.toFixed(3) + '%';
+        createSpanNode(cellNameEl, spreadAndPrice);
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   function toFixed(number) {
@@ -72,17 +100,18 @@
   }
 
   function updatePrice() {
-    portfolioEl = document.querySelector('.main-app-view .p-portfolio .portfolio-overview');
-    marketEl = document.querySelector('.table-body.market');
-    if (marketEl) {
+    const href = window.location.href;
+    if (href.indexOf('/watchlists') > 0) {
       initWatchListView();
-    } else if (portfolioEl) {
+    } else if (href.indexOf('/portfolio') > 0) {
       initialPortfolioOverview();
     }
   }
+
   function start() {
     let options = getOptions();
     setInterval(updatePrice, options.refreshTime);
   }
+
   start();
 }());
