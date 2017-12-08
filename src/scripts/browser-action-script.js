ccc570a0e253ed6de096ccb1a150db7797479fb0
Duc Nguyen <vanducld@gmail.com>
2017-12-08 14:47:40 -0600

    Correct option page

--- src/scripts/browser-action-script.js
+++ src/scripts/browser-action-script.js
@@ -4,7 +4,7 @@ main();
 
 function main() {
   let currentUrl = window.location.href;
-  if (currentUrl.indexOf('etoro.com') === -1) {
+  if (currentUrl.indexOf('etoro.com') === -1 || !currentUrl) {
     window.location.href = 'https://www.etoro.com/watchlists';
   }
 }
